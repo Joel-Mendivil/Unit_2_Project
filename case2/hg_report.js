ceadb0badf731168d88e740c31e5c655f60a5831
Joel Mendivil <jmendivil171@west-mec.org>
2020-02-04 14:41:47 -0700

    Finished Case 2

--- case2/hg_report.js
+++ case2/hg_report.js
@@ -12,4 +12,28 @@
 	
 */
 
-var gameReport = "<h1>title</h1><h2>By: manufacturer</h2><img src ='hg_id.png' alt='id' id='gameImg' /><table><tr><th>Product ID></th><td>id</td></tr><tr><th>List Price</th><td>price</td></tr><tr><th>Platform</th><td>platform</td></tr><tr><th>ESRB Rating</th><td>esrb</td></tr><tr><th>Condition</th><td>condition</td></tr><tr><th>Release</th><td>release</td></tr></table>summary";
+var gameReport = "<h1>" + itemTitle + "</h1><h2>By: " + itemManufacturer + "</h2><img src ='hg_" + itemID + ".png' alt='" + 
+   itemID +"' " + itemID + "='gameImg' /><table><tr><th>Product ID</th><td>" + itemID + "</td></tr><tr><th>List Price</th><td>" + itemPrice + 
+   "</td></tr><tr><th>Platform</th><td>" + itemPlatform + "</td></tr><tr><th>ESRB Rating</th><td>" + itemESRB + 
+   "</td></tr><tr><th>Condition</th><td>" + itemCondition + "</td></tr><tr><th>Release</th><td>" + itemRelease + "</td></tr></table>" + 
+   itemSummary;
+document.getElementsByTagName("article")[0].innerHTML = gameReport;
+
+var ratingsSum = 0;
+var ratingsCount = ratings.length;
+for(var i = 0; i<ratings.length; i++){
+   ratingsSum += ratings[i];
+}
+var ratingAvg = ratingsSum/ratingsCount;
+var ratingReport = "<h1>Customer Reviews</h1><h2>" + ratingAvg + " out of 5 stars (count reviews)</h2>";
+
+for(var i = 0; i<3; i++){
+   ratingReport += "<div class='review'><h1>" + ratingTitles[i] + "</h1><table><tr><th>By</th><td>" + ratingAuthors[i] + "</td></tr><tr><th>Review Date</th><td>" + 
+      ratingDates[i] + "</td></tr><tr><th>Rating</th><td>";
+   for(var j = 1; j<=ratings[i]; j++){
+      ratingReport += "<img src='hg_star.png' />"
+   }
+   ratingReport += "</td></tr></table>" + ratingSummaries[i] + "</div>";
+}
+
+document.getElementsByTagName("aside")[0].innerHTML = ratingReport;
